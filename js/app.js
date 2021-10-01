3afadc357bd397a0bb24bb123ef4e917ccb686cc
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-09-30 22:09:09 -0600

    js created an element and added to body

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
-//document.body.style.backgroundColor = "red";
-//test me
+for(let i = 0; i < 50; i++) {
+    const div = document.createElement('div');
+    div.innerText = `Hello ${i +1}`;
+    document.body.append(div);
+}
